92c2f37bdb4f2ea8e6313f43a9e910c70c1cac15
Davide <davideno2000@gmail.com>
2026-09-14 15:37:35 +0200

    Add SQLAlchemy engine and session setup

---
+++ app/database.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from app.config import DATABASE_URL
+
+# Create the SQLAlchemy engine which configure the database connection
+engine = create_engine(DATABASE_URL)
+
+# Create a new instance of a sessionmaker, which will be used to create new sessions for interacting with the database
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Create a base class. Our future models will inherit from this class.
+Base = declarative_base()

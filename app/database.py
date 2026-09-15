23d5549301b0e99c47d45c43d929b3c062649373
Davide <davideno2000@gmail.com>
2026-09-15 11:16:51 +0200

    Add get_db function for Database Sessions

--- app/database.py
+++ app/database.py
@@ -10,3 +10,10 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Create a base class. Our future models will inherit from this class.
 Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()

e5914c82e1e7a45866a82ca9570994a1a414e0ed
Davide <davideno2000@gmail.com>
2026-09-15 15:54:06 +0200

    Add two test endpoint: create_user, read_users

--- app/main.py
+++ app/main.py
@@ -1,7 +1,21 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+
+from sqlalchemy.orm import Session
+
+from app.database import get_db
+from app.schemas import UserOut, UserCreate
+from app import crud
 
 app = FastAPI()
 
 @app.get("/health")
 def health_check():
     return {"status": "OK, the server is running!"}
+
+@app.post("/users", response_model=UserOut)
+def create_user(user: UserCreate, db: Session = Depends(get_db)):
+    return crud.create_user(db, user)
+
+@app.get("/users", response_model=list[UserOut])
+def read_users(db: Session = Depends(get_db)):
+    return crud.get_users(db)

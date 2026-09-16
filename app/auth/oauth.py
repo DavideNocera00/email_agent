0da8320ac40d4625f429bbd92c5537b492da0656
Davide <davideno2000@gmail.com>
2026-09-16 10:46:06 +0200

    Add get_valid_access_token to obtain an access_token

--- app/auth/oauth.py
+++ app/auth/oauth.py
@@ -1,4 +1,7 @@
 from google_auth_oauthlib.flow import Flow
+from google.oauth2.credentials import Credentials
+from google.auth.transport.requests import Request as GoogleRequest
+
 from app.config import GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI, GOOGLE_SCOPES
 
 import requests
@@ -51,3 +54,16 @@ def get_user_email(access_token: str) -> str:
     )
     response.raise_for_status()
     return response.json()["email"]
+
+# We build a Credentials object with the refresh token and we contact Google's token endpoint using the client_ID and client_secret.
+# The function return the new access_token that can be used to access the user's Gmail data.
+def get_valid_access_token(refresh_token: str) -> str:
+    credentials = Credentials(
+        token=None,
+        refresh_token=refresh_token,
+        token_uri="https://oauth2.googleapis.com/token",
+        client_id=GOOGLE_CLIENT_ID,
+        client_secret=GOOGLE_CLIENT_SECRET,
+    )
+    credentials.refresh(GoogleRequest())
+    return credentials.token
